07f7985f981a092c7bfa775047f8339a60d2e8a8
Louis Warren <louis@lsw.nz>
2022-10-09 17:03:57 +1300

    Tidy up password entry

--- common.c
+++ common.c
@@ -68,36 +68,43 @@ show_fingerprint(const uint8_t public[32])
 
 static
 int
-set_no_echo(struct termios *prev, int fd)
+tty_set_no_echo(struct termios *tmp, int fd)
 {
 	struct termios term_noecho;
-	if (tcgetattr(fd, prev))
+	if (tcgetattr(fd, tmp))
 		return 1;
-	term_noecho = *prev;
+	term_noecho = *tmp;
 	term_noecho.c_lflag &= (tcflag_t)~ECHO;
 	if (tcsetattr(fd, TCSAFLUSH, &term_noecho))
 		return 1;
 	return 0;
 }
 
+static
+void
+tty_unset_no_echo(struct termios *tmp, int fd)
+{
+	(void)tcsetattr(fd, TCSAFLUSH, tmp);
+}
+
 uint32_t
 read_password(uint8_t *buf, uint32_t bufsize, const char *ttypath)
 {
 	uint32_t password_len = 0;
-	struct termios term_old;
+	struct termios tmp;
 	FILE *tty = fopen(ttypath, "r+");
 
 	if (!tty)
 		die("Failed to get a password from %s", ttypath);
 
-	if (set_no_echo(&term_old, tty->_fileno)) {
+	if (tty_set_no_echo(&tmp, tty->_fileno)) {
 		buf = fgets(buf, (int)bufsize, tty);
 	} else {
 		fprintf(tty, "Passphrase (Echo off): ");
 		fflush(tty);
 		buf = fgets(buf, (int)bufsize, tty);
 		fprintf(tty, "\n");
-		(void)tcsetattr(0, TCSAFLUSH, &term_old);
+		tty_unset_no_echo(&tmp, tty->_fileno);
 	}
 	fclose(tty);
 
