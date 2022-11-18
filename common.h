673f9d8c6ba054a03b976a4a897fe74522120e8c
Louis Warren <louis@lsw.nz>
2022-11-19 10:29:33 +1300

    Remove common

--- common.h
+++
@@ -1,6 +0,0 @@
-#define BLOCKSIZE (64 * 1024 * 1024)
-
-void show_fingerprint(const uint8_t public[32]);
-
-void nonce_inc(uint8_t nonce[24]);
-uint32_t read_password(uint8_t *buf, uint32_t bufsize, FILE *tty);
