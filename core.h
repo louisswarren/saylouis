2beba53e927135e2d2bf8ec22b36df62580eea88
Louis Warren <louis@lsw.nz>
2023-07-07 22:12:41 +1200

    Rewrite core.h

--- core.h
+++ core.h
@@ -1,21 +1,18 @@
+struct __attribute__((packed)) keyfile {
+	uint8_t preamble[32];
+	uint8_t nb_blocks_raw[4];
+	uint8_t nb_passes_raw[4];
+	uint8_t salt[16];
+	uint8_t nonce[24];
+	uint8_t key_encrypted[32];
+	uint8_t mac[16];
+};
+
 int
 getrandom_atomic(void *buf, size_t buflen)
 {
 	return getrandom(buf, buflen, 0) != buflen;
 }
-
-enum keyfile_offset {
-	/* Covered by AD */
-	KEYFILE_PREAMBLE        =   0, // 32 bytes
-	KEYFILE_BLOCKS          =  32, // 4 bytes BE
-	KEYFILE_PASSES          =  36, // 4 bytes BE
-	KEYFILE_SALT            =  40, // 16 bytes
-	/* End of AD */
-	KEYFILE_NONCE           =  56, // 24 bytes
-	KEYFILE_KEY_ENCRYPTED   =  80, // 32 bytes
-	KEYFILE_MAC             = 112, // 16 bytes
-	KEYFILE_END             = 128,
-};
 
 uint32_t
 u32_from_bytes(const uint8_t bytes[4])
@@ -36,16 +33,17 @@ bytes_from_u32(uint8_t bytes[4], uint32_t n)
 }
 
 int
-read_key(uint8_t key[32], const uint8_t *pass, uint32_t pass_size, FILE *f) {
-	uint8_t contents[KEYFILE_END];
+read_key(uint8_t key[32], const uint8_t *pass, uint32_t pass_size, FILE *f)
+{
+	struct keyfile c;
 	uint8_t derived_secret[32];
 	uint8_t *workarea;
 
-	if (!fread(contents, sizeof(contents), 1, f))
+	if (!fread(&c, sizeof(c), 1, f))
 		return 1;
 
-	uint32_t blocks = u32_from_bytes(contents + KEYFILE_BLOCKS);
-	uint32_t passes = u32_from_bytes(contents + KEYFILE_PASSES);
+	uint32_t blocks = u32_from_bytes(c.nb_blocks_raw);
+	uint32_t passes = u32_from_bytes(c.nb_passes_raw);
 
 	if (!blocks || !passes)
 		return 1;
@@ -61,9 +59,9 @@ read_key(uint8_t key[32], const uint8_t *pass, uint32_t pass_size, FILE *f) {
 
 	crypto_argon2_inputs inputs = {
 		.pass = pass,
-		.salt = contents + KEYFILE_SALT,
+		.salt = c.salt,
 		.pass_size = pass_size,
-		.salt_size = KEYFILE_NONCE - KEYFILE_SALT};
+		.salt_size = sizeof(c.salt)};
 
 	crypto_argon2(
 		derived_secret, sizeof(derived_secret),
@@ -75,13 +73,13 @@ read_key(uint8_t key[32], const uint8_t *pass, uint32_t pass_size, FILE *f) {
 
 	int unlock_status = crypto_aead_unlock(
 		key,
-		contents + KEYFILE_MAC,
+		c.mac,
 		derived_secret,
-		contents + KEYFILE_NONCE,
-		contents + KEYFILE_PREAMBLE,
-		KEYFILE_NONCE - KEYFILE_PREAMBLE,
-		contents + KEYFILE_KEY_ENCRYPTED,
-		KEYFILE_MAC - KEYFILE_KEY_ENCRYPTED);
+		c.nonce,
+		c.preamble,
+		c.nonce - c.preamble,
+		c.key_encrypted,
+		sizeof(c.key_encrypted));
 	crypto_wipe(derived_secret, sizeof(derived_secret));
 
 	return unlock_status;
@@ -98,21 +96,21 @@ write_key(
 	uint32_t nb_passes,
 	FILE *f)
 {
-	uint8_t contents[KEYFILE_END] = {0};
+	struct keyfile c = {0};
 	uint8_t derived_secret[32];
 	uint8_t *workarea;
 
-	memcpy(
-		contents + KEYFILE_PREAMBLE,
-		preamble,
-		preamble_size > 32 ? 32 : preamble_size);
+	if (preamble_size > sizeof(c.preamble)) {
+		memcpy(c.preamble, preamble, sizeof(c.preamble));
+	} else {
+		memcpy(c.preamble, preamble, preamble_size);
+	}
 
-	bytes_from_u32(contents + KEYFILE_BLOCKS, nb_blocks);
-	bytes_from_u32(contents + KEYFILE_PASSES, nb_passes);
+	bytes_from_u32(c.nb_blocks_raw, nb_blocks);
+	bytes_from_u32(c.nb_passes_raw, nb_passes);
 
 	/* Random salt + nonce */
-	size_t randlen = KEYFILE_KEY_ENCRYPTED - KEYFILE_SALT;
-	if (getrandom_atomic(contents + KEYFILE_SALT, randlen))
+	if (getrandom_atomic(c.salt, c.key_encrypted - c.salt))
 		return 1;
 
 	if (!(workarea = calloc(nb_blocks, 1024)))
@@ -126,9 +124,9 @@ write_key(
 
 	crypto_argon2_inputs inputs = {
 		.pass = pass,
-		.salt = contents + KEYFILE_SALT,
+		.salt = c.salt,
 		.pass_size = pass_size,
-		.salt_size = KEYFILE_NONCE - KEYFILE_SALT};
+		.salt_size = sizeof(c.salt)};
 
 	crypto_argon2(
 		derived_secret, sizeof(derived_secret),
@@ -139,17 +137,17 @@ write_key(
 	free(workarea);
 
 	crypto_aead_lock(
-		contents + KEYFILE_KEY_ENCRYPTED,
-		contents + KEYFILE_MAC,
+		c.key_encrypted,
+		c.mac,
 		derived_secret,
-		contents + KEYFILE_NONCE,
-		contents + KEYFILE_PREAMBLE,
-		KEYFILE_NONCE - KEYFILE_PREAMBLE,
+		c.nonce,
+		c.preamble,
+		c.nonce - c.preamble,
 		key,
-		KEYFILE_MAC - KEYFILE_KEY_ENCRYPTED);
+		sizeof(c.key_encrypted));
 	crypto_wipe(derived_secret, sizeof(derived_secret));
 
-	if (!fwrite(contents, sizeof(contents), 1, f))
+	if (!fwrite(&c, sizeof(c), 1, f))
 		return 1;
 
 	return 0;
